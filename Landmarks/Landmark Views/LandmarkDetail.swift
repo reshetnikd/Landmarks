47c7f933d2f179be0912441b11690ed46e37b399
reshetnikd <dmitry.reshetnik@icloud.com>
2020-03-28 00:08:30 +0200

    Composing Complex Interfaces

--- Landmarks/Landmark Views/LandmarkDetail.swift
+++ Landmarks/Landmark Views/LandmarkDetail.swift
@@ -59,7 +59,7 @@ struct LandmarkDetail: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkDetail(landmark: landmarkData[0])
             .previewDevice(PreviewDevice(rawValue: "iPhone SE"))
